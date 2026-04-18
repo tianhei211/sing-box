15138fd95b266281a1c33dff3d7fa87b51221446
tianhei <wqmlovesky@gmail.com>
2026-04-18 13:33:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -177,7 +177,7 @@ download() {
         is_ok=$is_core_ok
         ;;
     sh)
-        link=https://github.com/tianhei211/sing-box/archive/refs/heads/main.tar.gz
+        link=https://github.com/tianhei211/sing-box/releases/tag/v1.16/code.tar.gz
         name="$is_core_name 脚本"
         tmpfile=$tmpsh
         is_ok=$is_sh_ok
