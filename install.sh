c4b527fc42a082fdaa64e10794b2836bb6654fc4
tianhei <wqmlovesky@gmail.com>
2026-04-18 13:40:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-author=tianhei211
-# github=https://github.com/tianhei211/sing-box
+author=233boy
+# github=https://github.com/233boy/sing-box
 
 # bash fonts colors
 red='\e[31m'
@@ -177,7 +177,7 @@ download() {
         is_ok=$is_core_ok
         ;;
     sh)
-        link=https://github.com/tianhei211/sing-box/releases/tag/v1.16/code.tar.gz
+        link=https://github.com/${is_sh_repo}/releases/latest/download/code.tar.gz
         name="$is_core_name 脚本"
         tmpfile=$tmpsh
         is_ok=$is_sh_ok
